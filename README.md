409c21b95e7f5e0a9759aea3892b7e5377adf6ec
Louis Bui <louisbui2000@gmail.com>
2021-06-27 02:22:50 -0700

    added regex for specific date, updated README, general improvements

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # c_journal
+
+Replacement to the original journal script I had written in Rust before. Function is the same, just made to blend with other Linux commands as a C binary.
+
+## How to Use
+
+It's best to add the journal binary to /usr/local/bin to have it as like a normal Linux command. You do that with a symbolic link using the command
+
+`sudo ln -s path_to_journal /usr/local/bin/`
+
+Command runs as follows:
+
+`journal [yesterday | twodaysago | 'date']
+
+yesterday: the previous day's entry.\
+twodaysago: the entry two days ago.\
+'date': enter a specific date and it will be accessed accordingly. CURRENTLY UNAVAILABLE
--- journal.c
+++ journal.c
@@ -2,34 +2,58 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <regex.h>
 
 // const number of seconds in a day
 const int DAY = 86400;
 
+// void get_date(char *buf, char *arg)
+// {
+// 	regex_t date_regex;
+// 	char *pattern = "^20[:number:][-/](0[:number:]|1[012])[-/](0[:number:]|[12][:number:]|3[01])$";
+// 	regcomp(&date_regex, pattern, 0);
+
+// 	if strn
+// }
+
+int reg_test(char *inp)
+{
+	regex_t date_regex;
+	regcomp(&date_regex, "^20[0-9][0-9][-/](0[1-9]|1[012])[-/](0[1-9]|[12][0-9]|3[01])$", REG_EXTENDED);
+	return regexec(&date_regex, inp, 0, NULL, 0) ? 0 : 1;
+}
+
 int main(int argc, char *argv[])
 {
-	
-	// formatting the date into a string
-	time_t rawtime = time(NULL);
-	if (argc > 1 && strncmp("yesterday", argv[1], 9) == 0)
-		rawtime -= DAY;
-
-	struct tm today = *localtime(&rawtime);
 	char date_str[20];
-	strftime(date_str, 20, "%Y/%B/%d\n", &today);
+	if (argc > 1 && reg_test(argv[1]))
+		strncpy(date_str, argv[1], 20);
+	else
+	{
+		time_t rawtime = time(NULL);
+		if (argc > 1)
+		{
+			if (strncmp("yesterday", argv[1], 10) == 0)
+				rawtime -= DAY;
+			else if (strncmp("twodaysago", argv[1], 11) == 0)
+				rawtime -= DAY * 2;
+		}
+		struct tm today = *localtime(&rawtime);
+		strftime(date_str, 20, "%Y/%B/%d\n", &today);
+	}
 
 	// putting the journal path together
 	char journal_path[90];
 	strncat(journal_path, getenv("HOME"), 50);
 	strncat(journal_path, "/Documents/journal/", 20);
 	strncat(journal_path, date_str, 20);
-	
+
 	// for when the file doesn't exist
-	
 
 	// executing the text editor on the journal path
 	char command[100] = "gedit ";
 	strncat(command, journal_path, 90);
+	printf("%s\n", command);
 	system(command);
 
 	return 0;
